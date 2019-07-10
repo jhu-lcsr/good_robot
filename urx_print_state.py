a6eb7d31ad501b4cc8051253a5298436f92e59fc
N Ouyang <nouyang@alum.mit.edu>
2019-07-10 17:44:49 -0300

    Corrected urx print state script, to os.exit all threads

--- urx_print_state.py
+++ urx_print_state.py
@@ -4,23 +4,34 @@ import urx
 import logging
 import time
 import numpy as np
+import sys
+import os
 
-tcp_host_ip = "10.75.15.94"
+
+tcp_host_ip = "10.75.15.91"
 
 if __name__ == "__main__":
     # logging.basicConfig(level=logging.WARN)
     np.set_printoptions(precision=4)
 
-    rob = urx.Robot(tcp_host_ip)
+    try:
+        rob = urx.Robot(tcp_host_ip)
+    except:
+        sys.exit()
     #rob = urx.Robot("localhost")
     rob.set_tcp((0, 0, 0, 0, 0, 0))
     rob.set_payload(0.5, (0, 0, 0))
-    while True:
-        time.sleep(1)
-        try:
+    try:
+        while True:
             rob = urx.Robot(tcp_host_ip)
             pose = rob.getl()
             print("robot tcp is at: ", np.array(pose), '\n')
-
-        finally:
-            rob.close()
+            time.sleep(0.05)
+    except:
+        print('caught exception')
+        print('closing robot')
+        rob.close()
+        # print('exiting')
+        # sys.exit()
+        print('exiting again')
+        os._exit(1)
