caa4e6e1950e02613372e029d992b38b7692d1a5
Andrew Hundt <athundt@gmail.com>
2019-08-30 22:57:34 -0400

    evaluate.py add code to evaluate preset cases with varying numbers of blocks in the scene

--- evaluate.py
+++ evaluate.py
@@ -11,13 +11,19 @@ parser = argparse.ArgumentParser(description='Plot performance of a session over
 parser.add_argument('--session_directory', dest='session_directory', action='store', type=str, help='path to session directory for which to measure performance')
 parser.add_argument('--method', dest='method', action='store', type=str, help='set to \'reactive\' (supervised learning) or \'reinforcement\' (reinforcement learning ie Q-learning)')
 parser.add_argument('--num_obj_complete', dest='num_obj_complete', action='store', type=int, help='number of objects picked before considering task complete')
+parser.add_argument('--preset', dest='preset', action='store_true', default=False, help='use the 11 preset object locations for difficult situations')
+parser.add_argument('--preset_num_trials', dest='preset_num_trials', action='store', type=int, default=10, help='How many trials did you run each preset situation for? (default is 10)')
+
 args = parser.parse_args()
 session_directory = args.session_directory
 method = args.method
 num_obj_complete = args.num_obj_complete
+# There is a preset directory with this number of objects each time
+num_obj_presets = [4, 5, 3, 5, 5, 6, 3, 6, 6, 5, 4]
+preset_trials = args.preset_num_trials
 
 # Parse data from session (action executed, reward values)
-# NOTE: reward_value_log just stores some value which is indicative of successful grasping, which could be a class ID (reactive) or actual reward value (from MDP, reinforcement) 
+# NOTE: reward_value_log just stores some value which is indicative of successful grasping, which could be a class ID (reactive) or actual reward value (from MDP, reinforcement)
 transitions_directory = os.path.join(session_directory, 'transitions')
 executed_action_log = np.loadtxt(os.path.join(transitions_directory, 'executed-action.log.txt'), delimiter=' ')
 max_iteration = executed_action_log.shape[0]
@@ -27,15 +33,19 @@ reward_value_log = reward_value_log[0:max_iteration]
 clearance_log = np.loadtxt(os.path.join(transitions_directory, 'clearance.log.txt'), delimiter=' ')
 max_trials = len(clearance_log)
 clearance_log = np.concatenate((np.asarray([0]), clearance_log), axis=0).astype(int)
-
+print('number of clearances: ' + str(len(clearance_log)-1))
 # Count number of pushing/grasping actions before completion
 num_actions_before_completion = clearance_log[1:(max_trials+1)] - clearance_log[0:(max_trials)]
 
 grasp_success_rate = np.zeros((max_trials))
 grasp_num_success = np.zeros((max_trials))
 grasp_to_push_ratio = np.zeros(max_trials)
-for trial_idx in range(1,len(clearance_log)):
-
+valid_clearance = []
+for trial_idx in range(1, len(clearance_log)):
+    if args.preset:
+        # TODO(ahundt) If a bug is fixed in the logging code, there is one too many trials here.
+        preset_situation_num = int((trial_idx-2)/preset_trials)
+        num_obj_complete = num_obj_presets[preset_situation_num]
     # Get actions and reward values for current trial
     tmp_executed_action_log = executed_action_log[clearance_log[trial_idx-1]:clearance_log[trial_idx],0]
     tmp_reward_value_log = reward_value_log[clearance_log[trial_idx-1]:clearance_log[trial_idx]]
@@ -51,12 +61,15 @@ for trial_idx in range(1,len(clearance_log)):
         tmp_num_grasp_success = np.sum(tmp_reward_value_log[tmp_grasp_attempt_ind] == 0) # Class ID for successful grasping is 0 (reactive policy)
     elif method == 'reinforcement':
         tmp_num_grasp_success = np.sum(tmp_reward_value_log[tmp_grasp_attempt_ind] >= 0.5) # Reward value for successful grasping is anything larger than 0.5 (reinforcement policy)
-    
+
     grasp_num_success[trial_idx-1] = tmp_num_grasp_success
     grasp_success_rate[trial_idx-1] = float(tmp_num_grasp_success)/float(len(tmp_grasp_attempt_ind))
-
-# Which trials reached task completion?
-valid_clearance = grasp_num_success >= num_obj_complete
+    # Did the trial reach task completion?
+    cleared = tmp_num_grasp_success >= num_obj_complete
+    if args.preset:
+        print('trial: ' + str(trial_idx) + ' preset_situation_num: ' + str(preset_situation_num) + ' num_obj: ' + str(num_obj_complete) +
+              ' num_grasps: ' + str(tmp_num_grasp_success) + ' cleared: ' + str(cleared))
+    valid_clearance.append(cleared)
 
 # Display results
 print('Average %% clearance: %2.1f' % (float(np.sum(valid_clearance))/float(max_trials)*100))
