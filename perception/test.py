2daf35e754cd2f944180f2f4c2f068c4ede70634
jaydenwu17 <hwu67@jhu.edu>
2019-10-14 16:48:06 -0400

    perception/test.py add continuous image update on PrimeSense

--- perception/test.py
+++ perception/test.py
@@ -1,4 +1,4 @@
-#! /src/env/bin python3
+#! /usr/bin/env python3
 
 '''
 
@@ -17,16 +17,17 @@ def main():
     PS = PrimesenseSensor()
     PS.start()
 
-    color_img, depth_img, _ = PS.frames()
-    print(f'color_img type: {color_img.raw_data.dtype}')
-    print(f'depth_img type: {depth_img.raw_data.dtype}')
+    while True:
+        color_img, depth_img, _ = PS.frames()
+        # print(f'color_img type: {color_img.raw_data.dtype}')
+        # print(f'depth_img type: {depth_img.raw_data.dtype}')
 
-    color_img_raw = cv2.cvtColor(color_img.raw_data, cv2.COLOR_BGR2RGB)
+        color_img_raw = cv2.cvtColor(color_img.raw_data, cv2.COLOR_BGR2RGB)
 
-    cv2.imshow('test_color.png', color_img_raw)
-    cv2.imshow('test_depth.png', depth_img.raw_data)
+        cv2.imshow('test_color.png', color_img_raw)
+        cv2.imshow('test_depth.png', depth_img.raw_data)
 
-    cv2.waitKey(0)
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
