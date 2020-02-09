9d1b0fbcde1dcc99f37cb6fe81e7cd0199d8ed0f
Nick Greene <njgreene3@gmail.com>
2020-02-09 15:35:37 -0500

    main.py fixed robot() parameters

--- main.py
+++ main.py
@@ -188,7 +188,8 @@ def main(args):
     # Initialize pick-and-place system (camera and robot)
     robot = Robot(is_sim, obj_mesh_dir, num_obj, workspace_limits,
                   tcp_host_ip, tcp_port, rtc_host_ip, rtc_port,
-                  is_testing, test_preset_cases, test_preset_file, place, grasp_color_task, unstack=unstack, heightmap_resolution)
+                  is_testing, test_preset_cases, test_preset_file,
+                  place, grasp_color_task, unstack=unstack, heightmap_resolution=heightmap_resolution)
 
     # Initialize trainer
     trainer = Trainer(method, push_rewards, future_reward_discount,
