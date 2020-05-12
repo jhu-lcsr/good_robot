5bf5925161ce12af31fc8a49a2c1f55a23b097e6
Andrew Hundt <athundt@gmail.com>
2020-05-12 16:14:01 -0400

    main.py plot title add Discounted Reward

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ def run_title(args):
         title += 'Push and Grasp, '
     if args.trial_reward:
         title += 'SPOT Trial Reward, '
+    elif args.discounted_reward:
+        title += 'Discounted Reward, '
     else:
         title += 'Two Step Reward, '
     if args.common_sense:
