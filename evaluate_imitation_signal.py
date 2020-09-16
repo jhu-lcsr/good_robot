af17494d8b2d3b63c71cfb0e43102b81be5f29d6
Adit Murali <adit.murali@outlook.com>
2020-09-16 15:35:37 -0400

    fixed isfile check input

--- evaluate_imitation_signal.py
+++ evaluate_imitation_signal.py
@@ -19,8 +19,10 @@ args = parser.parse_args()
 
 # make dir for imitation action visualizations if save_visualizations are set
 if args.save_visualizations:
-    depth_heightmap_list = sorted([f for f in os.listdir(os.path.join(args.log_home, 'data', 'depth-heightmaps')) if os.path.isfile(f)])
-    rgb_heightmap_list = sorted([f for f in os.listdir(os.path.join(args.log_home, 'data', 'color-heightmaps')) if os.path.isfile(f)])
+    depth_heightmap_list = sorted([f for f in os.listdir(os.path.join(args.log_home, 'data', 'depth-heightmaps')) \
+            if os.path.isfile(os.path.join(args.log_home, 'data', 'depth-heightmaps', f))])
+    rgb_heightmap_list = sorted([f for f in os.listdir(os.path.join(args.log_home, 'data', 'color-heightmaps')) \
+            if os.path.isfile(os.path.join(args.log_home, 'data', 'color-heightmaps', f))])
     depth_home_dir = os.path.join(args.log_home, 'data', 'depth-heightmaps', 'im_depth_signal')
     rgb_home_dir = os.path.join(args.log_home, 'data', 'color-heightmaps', 'im_rgb_signal')
     os.makedirs(depth_home_dir)
