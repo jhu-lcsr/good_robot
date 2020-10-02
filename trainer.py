6e4d848c909ba1f5462376572d71b70bb0a264d7
Adit Murali <adit.murali@outlook.com>
2020-10-02 11:06:40 -0400

    fixed bug in demo_mask option

--- trainer.py
+++ trainer.py
@@ -533,14 +533,19 @@ class Trainer(object):
             # TODO(ahundt) "common sense" dynamic action space parameters should be accessible from the command line
             # "common sense" dynamic action space, mask pixels we know cannot lead to progress
             if keep_action_feat:
-                # no demo and keeping action feat so have feat vectors to mask in addition
-                # to final predictions
+                # only mask action feature maps from robot obs if demo_mask is set
                 if not use_demo:
-                    push_feat, grasp_feat, place_feat = utils.common_sense_action_space_mask(depth_heightmap,
-                            push_feat, grasp_feat, place_feat, self.place_dilation, self.show_heightmap, color_heightmap)
+                    if demo_mask:
+                        push_feat, grasp_feat, place_feat = utils.common_sense_action_space_mask(depth_heightmap,
+                                push_feat, grasp_feat, place_feat, self.place_dilation, self.show_heightmap, color_heightmap)
+                    else:
+                        push_feat = np.ma.masked_array(push_predictions)
+                        grasp_feat = np.ma.masked_array(grasp_predictions)
+                        if place:
+                            place_feat = np.ma.masked_array(place_predictions)
 
                 # using demo and demo mask is set, so common sense mask predictions
-                if use_demo and demo_mask:
+                elif demo_mask:
                     push_predictions, grasp_predictions, place_predictions = utils.common_sense_action_space_mask(depth_heightmap,
                             push_predictions, grasp_predictions, place_predictions, self.place_dilation, self.show_heightmap, color_heightmap)
                 
@@ -551,6 +556,7 @@ class Trainer(object):
                     grasp_predictions = np.ma.masked_array(grasp_predictions)
                     if self.place:
                         place_predictions = np.ma.masked_array(place_predictions)
+
             else:
                 push_predictions, grasp_predictions, place_predictions = utils.common_sense_action_space_mask(depth_heightmap,
                         push_predictions, grasp_predictions, place_predictions, self.place_dilation, self.show_heightmap, color_heightmap)
