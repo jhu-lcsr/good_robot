db5aec20bd32ead728e30186a0755dd1347324b4
Zooey He AMD <zhuohonghe@yahoo.com>
2021-02-26 01:05:11 -0500

    bug fixes

--- robot.py
+++ robot.py
@@ -2199,16 +2199,16 @@ class Robot(object):
                 # print('bottom_pos:', bottom_pos)
                 # print('top_pos:', top_pos)
                 # print('distance_threshold: ', distance_threshold)
-            if top_pos[2] < (bottom_pos[2] + vert_distance_threshold / 2.0):
-                if not self.grasp_color_task:
-                    print('check_stack(): not high enough for idx: ' + str(idx))
-                working_seq_found=False
-                max_height=max(max_height, idx + 1)
+                if top_pos[2] < (bottom_pos[2] + vert_distance_threshold / 2.0):
+                    if not self.grasp_color_task:
+                        print('check_stack(): not high enough for idx: ' + str(idx))
+                    working_seq_found=False
+                    max_height=max(max_height, idx + 1)
 
                 # Check that the blocks are near each other
                 dist = np.linalg.norm(np.array(bottom_pos) - np.array(top_pos))
                 # print('distance: ', dist)
-            if dist > vert_distance_threshold:
+                if dist > vert_distance_threshold:
                     if not self.grasp_color_task:
                         print('check_stack(): too far apart')
                     working_seq_found=False
--- utils.py
+++ utils.py
@@ -304,7 +304,7 @@ def process_prediction_language_masking(language_data, predictions, show_heightm
     if show_heightmap:
         # visualize the common sense function results
         # show the heightmap
-        plt.figure()
+        # plt.figure()
         fig, ax = plt.subplots(2,3)
         ax[0,0].imshow(curr_mask[0,:,:])
         ax[0,1].imshow(1 - language_mask[0,:,:])
