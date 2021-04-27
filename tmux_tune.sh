ba529fa0809fe128a46d2ff6ca2126a26b27aade
esteng <elias.stengel@gmail.com>
2021-04-27 11:17:50 -0400

    pre-pull

--- tmux_tune.sh
+++ tmux_tune.sh
@@ -4,6 +4,6 @@ for num in $(seq 0 7);
 do 
     #tmux new-session -d -s "test${num}" "bash execute_tune.sh ${num}";
     tmux new-session -d -s "test${num}" "bash execute_tune_gr.sh ${num}";
-    sleep 60;
+    sleep 300;
 done;
 
