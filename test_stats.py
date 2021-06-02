e450423aef5a691399d41e2f3184ac1d6d71802a
Andrew Hundt <ATHundt@gmail.com>
2021-06-02 18:38:17 -0400

    test_stats.py unstack stats custom log bugfixes

--- test_stats.py
+++ test_stats.py
@@ -1,6 +1,7 @@
 # test_stats.py collects trial avg progress reversal/recovery stats
 import cv2
 import os
+import re
 import argparse
 import numpy as np
 from utils import get_prediction_vis
@@ -46,6 +47,8 @@ if __name__ == '__main__':
         clearance_log = np.loadtxt(os.path.join(args.data_dir, 'transitions', 'clearance.log.txt'), **kwargs).astype(np.int64)
         clearance_log = clearance_log.tolist()
 
+    unstack = re.search('unstack', args.data_dir, re.IGNORECASE)
+
     # update the reward values for a whole trial, not just recent time steps
     end = int(clearance_log[-1][0])
     clearance_length = len(clearance_log)
@@ -63,9 +66,13 @@ if __name__ == '__main__':
     recoveries = []
     successful_trials = []
     trial_start = 0
+    efficiency_actions_six = 0
+    efficiency_actions_four = 0
     for i in range(num_trials):
         trial_successful = 0
         trial_end = clearance_log[i][0]
+        progress_reversal = 0.
+        stack_height = 1
         print('----------\ntrial num: ' + str(i))
         print('start: ' + str(trial_start) + ' trial end: ' + str(trial_end ))
         if trial_start == trial_end:
@@ -73,17 +80,30 @@ if __name__ == '__main__':
             continue
         trial_heights = np.array(stack_height_log[trial_start: trial_end])
         print(trial_heights)
-        max_heights += [np.max(trial_heights)]
-        stack_height = 1
-        progress_reversal = 0.
+        if unstack and len(trial_heights) == 1:
+            # special case for unstacking task where the trial ends immediately
+            progress_reversal = 1.
+            max_heights += [1]
+        else:
+            max_heights += [np.max(trial_heights)]
+        
+        # workaround trials that end in fewer actions than it would be possible to optimally succeed
+        efficiency_actions_six += max(len(trial_heights), 6)
+        efficiency_actions_four += max(len(trial_heights), 4)
+
         for j in range(trial_start, max(trial_start, trial_end - 1)):
             # allow a little bit of leeway, 0.1 progress, to consider something a reversal
             if stack_height_log[j][0] - epsilon > stack_height_log[j+1][0]:
                 progress_reversal = 1.
                 print('trial ' + str(i) + ' progress reversal at overall step: ' + str(j) + ' (this trial step: ' + str(j - trial_start) + ') because ' + 
-                      str(stack_height_log[j][0] ) + ' - ' + str(epsilon) + ' > ' + str(stack_height_log[j+1][0]))
+                    str(stack_height_log[j][0] ) + ' - ' + str(epsilon) + ' > ' + str(stack_height_log[j+1][0]))
         progress_reversals += [progress_reversal]
         trial_successful = trial_success_log[trial_end] > trial_success_log[trial_start]
+        print('log indicates trial success') if trial_successful else print('log indicates trial failure')
+        if unstack and not trial_successful and len(trial_heights) > 1 and max_heights[-1] == 4.:
+            # special case for unstack failures, sometimes progress of 4 gets logged after a topple
+            print('unstack correction max height: ' + str(max_heights[-1]) + ' to ' + str(trial_heights[-2]))
+            max_heights[-1] = float(trial_heights[-2])
         successful_trials += [trial_successful]
         if progress_reversal == 1.:
             recoveries += [1.] if trial_successful == 1 else [0.]
@@ -102,10 +122,10 @@ if __name__ == '__main__':
     print('avg max height: ' + str(np.mean(max_heights)) + ' (higher is better, find max height for each trial, then average those values)')
     print('avg max progress: ' + str(np.mean(np.rint(max_heights))/success_height) + ' (higher is better,  (avg(round(max_heights))/' + str(success_height) + '))')
     print('avg reversals: ' + str(np.mean(progress_reversals)) + ' (lower is better)')
-    print('avg recoveries : ' + str(np.mean(recoveries)) + ' (higher is better, no need for recovery attempts is best)')
+    print('avg recoveries: ' + str(np.mean(recoveries)) + ' (higher is better, no need for recovery attempts is best)')
     print('avg logged trial success: ' + str(np.mean(successful_trials)) + " (successful trials according to trial_success_log.txt)")
     print('avg trial success: ' + str(np.mean(max_heights >= (success_height - epsilon))) + ' (higher is better,  (success_height - epsilon) height or higher)')
-    print('action efficiency with 6 action per trial optimum: ' + str((6.*num_trials)/clearance_log[num_trials][0]) + ' action efficiency with 4 action per trial optimum: ' + str((4.*num_trials)/clearance_log[num_trials][0]))
+    print('action efficiency with 6 action per trial optimum: ' + str((6.*num_trials)/efficiency_actions_six) + ' action efficiency with 4 action per trial optimum: ' + str((4.*num_trials)/efficiency_actions_four))
     print('data dir: ' + str(args.data_dir))
 
 
