b73460d09438cd2c9c595bb48bccecf310b9334c
Adit <adit.murali@outlook.com>
2021-08-28 21:32:10 +0200

    trial_rewards imp IN PROGRESS

--- train_offline.py
+++ train_offline.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--task_type', default='stack', help='stack/row/unstack/vertical_square')
     parser.add_argument('-o', '--out_dir', default=None, help='where to write finetuned model, WILL NOT SAVE IF BLANK')
     parser.add_argument('-l', '--learning_rate', default=1e-5, help="What learning rate to use?")
+    parser.add_argument('--trial_reward', default=False, aciton='store_true', help='use trial reward?')
     parser.add_argument('--future_reward_discount', dest='future_reward_discount', type=float, action='store', default=0.65)
     args = parser.parse_args()
 
@@ -111,12 +112,15 @@ if __name__ == '__main__':
 
         # multiplier is progress for grasp, progress + 1 (next_progress) for place
         multiplier = progress if grasp_success else next_progress
-        reward, old_reward = trainer.get_label_value(action_str, push_success=False,
-                grasp_success=grasp_success, change_detected=True, prev_push_predictions=None,
-                prev_grasp_predictions=None, next_color_heightmap=next_color_heightmap,
-                next_depth_heightmap=next_depth_heightmap, color_success=None,
-                goal_condition=None, place_success=place_success, prev_place_predictions=None,
-                reward_multiplier=multiplier)
+        if not trial_reward:
+            reward, old_reward = trainer.get_label_value(action_str, push_success=False,
+                    grasp_success=grasp_success, change_detected=True, prev_push_predictions=None,
+                    prev_grasp_predictions=None, next_color_heightmap=next_color_heightmap,
+                    next_depth_heightmap=next_depth_heightmap, color_success=None,
+                    goal_condition=None, place_success=place_success, prev_place_predictions=None,
+                    reward_multiplier=multiplier)
+        else:
+            reward = trial_rewards[progress]
 
         # training step
         loss = trainer.backprop(color_heightmap, valid_depth_heightmap, action_str,
